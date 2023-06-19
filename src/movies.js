24fe76ae70946c5d64cde5301659399ddf42cf9a
lipamaoa <filipa.ferreirasilva@gmail.com>
2023-06-19 22:24:08 +0100

    second commit with bonus questions

--- src/movies.js
+++ src/movies.js
@@ -1,9 +1,12 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
+
 function getAllDirectors(moviesArray) {
 
-    const allDirectors = moviesArray.map(item => item.director);
+    const allDirectors = moviesArray
+        .map(item => item.director)
+        .filter((item, index, arr) => arr.indexOf(item) === index);
     return allDirectors;
 }
 
@@ -65,11 +68,21 @@ function orderAlphabetically(moviesArray) {
 function turnHoursToMinutes(moviesArray) {
 
     const convertedMovies = moviesArray.map(item => {
-        const time = item.duration.split(" ");
-        const hours = Number(time[0]);
-        const minutes = Number(time[1]);
 
-        const totalMinutes = hours * 60 + minutes;
+        const parts = item.duration.split(" ");
+
+        let totalMinutes = 0;
+
+        parts.forEach(part => {
+            if (part.includes("h")) {
+                const hours = parseInt(part.replace("h", ""));
+                totalMinutes += hours * 60;
+            }
+            else if (part.includes("m")) {
+                const minutes = parseInt(part.replace("m", ""));
+                totalMinutes += minutes;
+            }
+        });
 
         return {
             ...item,
@@ -82,4 +95,34 @@ function turnHoursToMinutes(moviesArray) {
 
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) { }
+function bestYearAvg(moviesArray) {
+    if (!moviesArray.length) {
+        return null;
+    }
+
+    let scores = {};
+
+    moviesArray.forEach(movie => {
+        if (scores[movie.year]) {
+            scores[movie.year].push(movie.score);
+        }
+        else {
+            scores[movie.year] = [movie.score];
+        }
+    });
+
+    let bestScore = 0;
+    let bestYear = "";
+
+    Object.keys(scores).forEach(year => {
+        const sum = scores[year].reduce((acc, score) => acc + score);
+        const average = sum / scores[year].length;
+
+        if (average > bestScore) {
+            bestScore = average;
+            bestYear = year;
+        }
+    })
+
+    return `The best year was ${bestYear} with an average score of ${bestScore}`
+}
